8c66aa12816cacf11e21b764172419c17e9e8ef8
Manash Baul <mximpaid@gmail.com>
2024-09-04 00:21:36 +0530

    Add psycopg2-binary dependency and initialize database in app_test.py

---
+++ src/app_test.py
@@ -0,0 +1,8 @@
+from src.constants import get_database
+from src.persistence import MODEL_LIST
+
+db = get_database()
+
+db.connect()
+db.create_tables(MODEL_LIST)
+db.close
--- src/constants.py
+++ src/constants.py
@@ -5,11 +5,13 @@ load_dotenv()
 
 config = dotenv_values(".env")
 
+
 def get_database():
     feedback_db = config.get("DATABASE")
-    host =  config.get('HOST')
-    port = config.get('PORT')
-    user = config.get('USER')
-    password = config.get('PASSWORD')
-    return PostgresqlDatabase(feedback_db, host = host, port = port, user = user, password= password)
-
+    host = config.get("HOST")
+    port = config.get("PORT")
+    user = config.get("USERNAME")
+    password = config.get("PASSWORD")
+    return PostgresqlDatabase(
+        feedback_db, host=host, port=port, user=user, password=password
+    )
